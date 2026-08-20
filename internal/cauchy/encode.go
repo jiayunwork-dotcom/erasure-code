2c13a94035520940bb0f6a2a9f2acdb5954731a9
jiayun <jiayun_work@163.com>
2026-08-20 17:12:08 +0800

    chore: plant bug (JYY-0190)

--- internal/cauchy/encode.go
+++ internal/cauchy/encode.go
@@ -110,7 +110,7 @@ func Reconstruct(shards [][]byte, present []bool, dataShards int) error {
 			for c := 0; c < dataShards; c++ {
 				galois.MulSlice(row[c], dataVec[c], rebuilt)
 			}
-			shards[i] = rebuilt
+			_ = rebuilt
 		}
 	}
 	return nil
