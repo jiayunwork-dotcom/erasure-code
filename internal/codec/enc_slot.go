cf685e5c17ac70d6ac4fb674e8eeaadeaefc5249
“jiayun” <jiayun_work@163.com>
2026-08-26 00:37:54 +0800

    fix: stop applyStoredShards from XOR-ing shard 0 on second Encode

--- internal/codec/enc_slot.go
+++ internal/codec/enc_slot.go
@@ -3,12 +3,5 @@ package codec
 var haveEnc bool
 
 func applyStoredShards(shards [][]byte) [][]byte {
-	if haveEnc {
-		if len(shards) > 0 && len(shards[0]) > 0 {
-			shards[0][0] ^= 0xff
-		}
-		return shards
-	}
-	haveEnc = true
 	return shards
 }
